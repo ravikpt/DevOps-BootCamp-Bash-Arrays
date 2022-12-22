41ac18fd4abd38b51309b8de4810409d90fc1553
Ranjit Kumar <95127402+ravikpt@users.noreply.github.com>
2022-12-22 13:22:22 +0400

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-# Place your code here
+#!/bin/bash
+
+
+folderNumber="$1"
+
+for((i=1; i<=$folderNumber; i++)); do
+  mkdir -p "folder"_$i
+ done
+
+echo "$folderNumber" folder created: && ls | grep folder
